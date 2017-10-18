103c8314a457c549e37288a7e705688ece94f9a6
Luna <luna@stor.lol>
2017-10-19 06:39:26 +1000

    increase delay until remuxing starts

--- registration.go
+++ registration.go
@@ -16,7 +16,7 @@ func RegisterSonarrEvent(event SonarrEvent) (err error) {
 	extension := filepath.Ext(event.EpisodeFile.RelativePath)
 	dir := filepath.Dir(event.Series.Path + "/" + event.EpisodeFile.RelativePath)
 	if extension == "mkv" || extension == ".mkv" {
-		time.Sleep(time.Second * 15) // conversion seems to get triggered too early
+		time.Sleep(time.Second * 120) // conversion seems to get triggered too early
 		RemuxMKVToMP4(dir, event.Series.Path+"/"+event.EpisodeFile.RelativePath)
 		SonarrClient.RescanSeries(event.Series.ID)
 	}
